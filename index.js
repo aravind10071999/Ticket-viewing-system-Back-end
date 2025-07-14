0c83376f8ff1dcbfa8945f341d15944f27ef688d
Aravindpsgst <aravind@psgsoftwaretechnologies.com>
2025-07-14 22:46:48 +0530

    included front end url in cors

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://ticket-viewing-system-front-end.onrender.com"
+}));
+
 app.use(express.json());
 
 // MongoDB connection
